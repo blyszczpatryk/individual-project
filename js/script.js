f7f2358a5c66b024cb0ce74bdc1e5ffa640d33b0
Patryk <blyszczpatryk@gmail.com>
2019-05-04 19:56:22 +0200

    Add content, add styles, add scripts

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ function toggleMenu(visible) {
   	document.querySelector('.menu').classList.toggle('show', visible)
 }
 
+function hideMenu(hidden) {
+	document.querySelector('.sidebar').classList.toggle('hide', hidden)
+}
+
 document.querySelector('.hamburger').addEventListener('click', function(e) {
   	e.preventDefault();
-  	toggleMenu()
+  	toggleMenu();
+  	hideMenu()
 });
